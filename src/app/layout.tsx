f1b92d5cd39d15f812ca2bef5db9e95f232457b3
aesthetics-of-record <111675679+aesthetics-of-record@users.noreply.github.com>
2025-10-11 05:41:22 +0900

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Alexandria } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const alexandria = Alexandria({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "600"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${alexandria.className}`}>{children}</body>
     </html>
   );
 }
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,57 @@
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+
+export default function SignInPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Create an account</CardTitle>
+          <CardDescription>
+            Using your preferred social provider or email and password
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex items-center justify-between">
+            <Button className="w-[45%]" variant="outline">
+              <FaGithub />
+              Github
+            </Button>
+            <Button className="w-[45%]" variant="outline">
+              <FcGoogle />
+              Google
+            </Button>
+          </div>
+          <div className="my-4 flex items-center gap-2">
+            <Separator className="flex-1" />
+            <div className="text-center text-muted-foreground text-sm">
+              Or continue with
+            </div>
+            <Separator className="flex-1" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Email</Label>
+            <Input placeholder="a@example.com" type="email" />
+            <Label>Password</Label>
+            <Input placeholder="********" type="password" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Create account</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
